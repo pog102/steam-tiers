e9e179eadc12725676f787c05e6c15935ade3566
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-07-04 15:34:05 +0300

    first

---
+++ README.md
@@ -0,0 +1,26 @@
+# Offline Tierlist Maker with Steam Implementation
+
+A simple implementation of [Tierlists](https://knowyourmeme.com/memes/tier-lists) for fetching owned Steam games.
+![image](./assets/image.png)
+
+## Prerequisites
+
+- Node.js
+
+## How to Use
+
+1. Edit `API_KEY` and `STEAM_ID` in `server.js`.
+
+   - To get Steam's API key, visit [Steam API Key](https://steamcommunity.com/dev/apikey).
+   - To find your Steam ID, visit [Steam ID Finder](https://www.steamidfinder.com/) and retrieve the `steamID64`.
+
+2. Install [Node.js](https://nodejs.org/en/download/prebuilt-binaries).
+
+3. Run the following command:
+
+   ```sh
+   node server.js
+   ```
+4. Go to `http://localhost:3000`
+
+5. Press the Steam icon and Done
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const axios = require('axios');
+
+
+const API_KEY = ''; //goto  https://steamcommunity.com/dev/apikey	
+const STEAM_ID = '';               //goto  https://www.steamidfinder.com/
+app.use(express.static(path.join(__dirname, '.')));
+
+app.post('/fetchGames', async (req, res) => {
+    try {
+        const response = await axios.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/', {
+            params: {
+                key: API_KEY,
+                steamid: STEAM_ID,
+             //   include_appinfo: true,
+                format: 'json'
+                
+            }
+        });
+
+         // Extract appid values from the response
+        // const games = response.data.response.games;
+         const games = response.data.response.games.filter(game => game.playtime_forever > 5);
+         const gameImages = games.map(game => `https://steamcdn-a.akamaihd.net/steam/apps/${game.appid}/header.jpg`);
+         
+         res.json({ status: 'success', gameImages });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', message: 'Error fetching owned games.' });
+    }
+});app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '.', 'index.html'));
+  });
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
